4f45a530dbbba33941028fc36f17506b57776b36
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-24 21:14:44 +0000

    task done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module that provides a function to concatenate two strings."""
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings str1 and str2."""
+    return (str1 + str2)
