d653db6d27626818c9a0263458d8739af2698f47
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-24 21:49:57 +0000

    task done

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""Module function to safely get a value from a dictionary."""
-
-
-from typing import Mapping, Any, Union, TypeVar
-
+"""Advanced type annotated function"""
+from typing import Any, Mapping, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """Return value associated with 'key' in 'dct' if it exists, else return 'default'."""
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
+    """More type annotations"""
     if key in dct:
         return dct[key]
     else:
