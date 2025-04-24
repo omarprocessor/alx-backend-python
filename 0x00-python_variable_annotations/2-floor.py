93645bf4972ea0fb41a7e65a460ca34a71f661e8
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-24 21:22:32 +0000

    task done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n as an integer."""
+    return (int(n))
